ba2a6e346da26fc7a247e1cf13168252b556632f
FrostTrauma <thedevguymarc@gmail.com>
2025-01-10 12:53:20 +0100

    working on de environment installer

---
+++ internal/installer/desktop-environment-installer.go
@@ -0,0 +1,20 @@
+package installer
+
+import (
+
+)
+
+func InstallDesktopEnvironment() {
+	// 1. Install hyperland and addons (waybar, hyperlock, wlogout, hyperpaper, hyperidle, hypersunset, hypercursor)
+
+	// 2. Utilitiy installation
+	//	2.1 notification daemon: mako
+	//	2.2 pipewire: pipewire + wireplumber
+	//	2.3 Desktop Portal: xdg-desktop-portal-hyprland
+	// 	2.4 Authentication agent: hyprpolkitagent
+	// 	2.5 QT Wayland Support:	qt5-wayland qt6-wayland
+	//	2.6 App Launchers: rofi
+	// 	2.7 File Managers: thunar, yazi
+	// 	2.8 Workspace management: hyprsome
+
+}
